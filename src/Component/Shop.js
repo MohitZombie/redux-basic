41cddc12d2899d17dc0f2a27ccfad69f232b37f6
MohitZombie <109340031+MohitZombie@users.noreply.github.com>
2022-07-17 00:17:44 +0530

    Update Shop.js

--- src/Component/Shop.js
+++ src/Component/Shop.js
@@ -14,20 +14,6 @@ const Shop = () => {
                 Mini bank
                 <button classNameName="btn" onClick={() => { depositMoney(100) }}>+</button>
             </div>
-            {/* <div className="container my-4">
-                <div className="row">
-                    <div className="column">
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">yum yum order now</p>
-                        <a href="/" className="btn btn-primary">ADD to Cart</a>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </>
     )
 }
